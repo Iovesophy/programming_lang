eb869932d842ac8017525294bb978b1943af1745
odennyuda <kazu825@me.com>
2016-09-18 13:43:34 +0900

    initial

--- brainf_ck.rb
+++ brainf_ck.rb
@@ -1,3 +1,27 @@
 class Brainf_ck
+	def initialize(src)
+		@tokens = src.chars.to_a
+	end
+
+	def run
+		tape = []
+		pc = 0
+		cur = 0
+
+		while pc < @tokens.size
+			case @tokens[pc]
+			when "+"
+				tape[cur] ||= 0
+				tape[cur] += 1
+			when "-"
+				tape[cur] ||= 0
+				tape[cur] -= 1
+			end
+
+			pc += 1
+		end
+	end
 	
 end
+
+Brainf_ck.new(ARGF.read).run
